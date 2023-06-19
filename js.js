680f26e538496fea45a66d0eaceb31a5fdb464c7
Irina00111 <128720563+Irina00111@users.noreply.github.com>
2023-06-19 14:30:19 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+
+/* This is a variable button declaration */
+const button = document.querySelector('button');
+
+/* Here we ckick on the button and then starts the logic */
+button.addEventListener('click', function() {
+	alert('Thank you for visiting my page!)))')
+})
